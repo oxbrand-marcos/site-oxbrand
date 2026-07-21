ca86168e308084e268c65ed5e5babffb9cf9112d
oxbrand-marcos <gestao@oxbrand.com.br>
2026-07-21 17:58:54 -0300

    fix(timeline): texto do marco Top 5% Kommo

--- components/about.tsx
+++ components/about.tsx
@@ -11,7 +11,7 @@ const timeline = [
   { year: '2023', event: 'Primeiros projetos internacionais, EUA e Europa', brand: false },
   { year: '2024', event: '+450 marcas atendidas e R$258M+ em faturamento gerado', brand: false },
   { year: '2025', event: 'Inauguração da Sede Própria Edifício Helbor Concept', brand: false },
-  { year: '2025', event: 'Parceiro Oficial Kommo e certificação Top 5% Brasil', brand: false },
+  { year: '2025', event: 'Parceiro Oficial Kommo, Top 5% dos parceiros no Brasil', brand: false },
   { year: '2026', event: 'Consolidação do método OX Growth Engineering e lançamento da nova plataforma digital', brand: false },
 ]
 
