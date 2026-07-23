9128f36fd4c6120432d564e99f70c171b6c1c787
oxbrand-marcos <gestao@oxbrand.com.br>
2026-07-23 18:18:38 -0300

    Home: cache-bust nas fotos do time (forca novo recorte retrato)

--- components/team.tsx
+++ components/team.tsx
@@ -50,7 +50,7 @@ export function Team() {
               {/* Foto */}
               <div className="relative h-72 overflow-hidden">
                 <Image
-                  src={member.image}
+                  src={`${member.image}?v=2`}
                   alt={`${member.name}, ${member.role} na OxBrand`}
                   fill
                   className="object-cover object-top transition-all duration-500"
