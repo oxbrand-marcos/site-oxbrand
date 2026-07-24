679800d9055e0d64e7341d1ed79254cad2a8967d
oxbrand-marcos <gestao@oxbrand.com.br>
2026-07-24 10:31:18 -0300

    LP: corrige endereco do mapa (Narciso Yague Guimaraes 1145)

--- app/agencia-de-marketing-digital-mogi-das-cruzes/page.tsx
+++ app/agencia-de-marketing-digital-mogi-das-cruzes/page.tsx
@@ -516,7 +516,7 @@ export default function LpMogiPage() {
               <div className="aspect-video border border-border overflow-hidden">
                 <iframe
                   title="Localização OxBrand, Mogi das Cruzes"
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3658.3!2d-46.1854!3d-23.5208!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjPCsDMxJzE0LjkiUyA0NsKwMTEnMDcuNCJX!5e0!3m2!1spt-BR!2sbr!4v1700000000000!5m2!1spt-BR!2sbr"
+                  src="https://maps.google.com/maps?q=Av.%20Ver.%20Narciso%20Yague%20Guimar%C3%A3es%2C%201145%20-%20Socorro%2C%20Mogi%20das%20Cruzes%20-%20SP%2C%2008780-500&output=embed"
                   width="100%"
                   height="100%"
                   style={{ border: 0 }}
