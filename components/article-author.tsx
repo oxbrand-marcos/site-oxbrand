36c21a6cac5f680ddbdbe71ecebd7bb9852c2ef6
oxbrand-marcos <gestao@oxbrand.com.br>
2026-07-28 10:18:09 -0300

    Add files via upload

--- components/article-author.tsx
+++ components/article-author.tsx
@@ -105,7 +105,7 @@ export const AUTHORS: Record<string, AuthorData> = {
     linkedin: OX_LINKEDIN, instagram: OX_INSTAGRAM, email: OX_EMAIL, local: LOCAL,
   },
   'murilo-novaes': { ...redator('murilo-novaes', 'Murilo Novaes'), email: undefined, linkedin: 'https://www.linkedin.com/in/murilo-novaes-de-oliveira-27a3191a2', instagram: 'https://www.instagram.com/murilo_novaesz', facebook: 'https://www.facebook.com/share/1D7ErKq5cP/' },
-  'breno-freire': redator('breno-freire', 'Breno Freire'),
+  'breno-freire': { ...redator('breno-freire', 'Breno Freire'), email: undefined, linkedin: 'https://www.linkedin.com/in/breno-freire-2a505a24b/', instagram: 'https://www.instagram.com/brenoofr_/' },
   'julia-rodrigues': redator('julia-rodrigues', 'Julia Rodrigues'),
   'maria-clara': { ...redator('maria-clara', 'Maria Clara'), email: undefined, linkedin: undefined, instagram: 'https://www.instagram.com/mariaclara_lecy', threads: 'https://www.threads.com/@mariaclara_lecy', twitter: 'https://x.com/clara_lecy' },
   'beatriz-soares': { ...redator('beatriz-soares', 'Beatriz Soares'), email: undefined, linkedin: 'https://www.linkedin.com/in/beatriz-soares-001823252', instagram: 'https://www.instagram.com/__beatrizsrm' },
